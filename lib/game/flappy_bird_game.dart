36927c31c16bf423031e442cb0627ae56a155812
aswinmv <aswinmv7@gmail.com>
2023-12-17 11:04:01 +0530

    physics added

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,12 +1,36 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:flutter_bird/components/background.dart';
 import 'package:flutter_bird/components/bird.dart';
 import 'package:flutter_bird/components/ground.dart';
+import 'package:flutter_bird/components/pipe_group.dart';
+import 'package:flutter_bird/game/config.dart';
 
-class FlappyBirdGame extends FlameGame {
+class FlappyBirdGame extends FlameGame with TapDetector, HasCollisionDetection {
   late Bird bird;
+  Timer interval = Timer(Config.pipeInterval, repeat: true);
   @override
   Future<void> onLoad() async {
-    addAll([Background(), Ground(), bird = Bird()]);
+    addAll([
+      Background(),
+      Ground(),
+      bird = Bird(),
+      PipeGroup(),
+    ]);
+
+    interval.onTick = () => add(PipeGroup());
+  }
+
+  @override
+  void onTap() {
+    super.onTap();
+    bird.fly();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    interval.update(dt);
   }
 }
